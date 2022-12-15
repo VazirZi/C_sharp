ad7b0587f5b07b817ef231d0fe92e65b59297654
VazirZi <vazir.zivar@mail.com>
2022-12-15 15:34:25 +0500

    Конструкция If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "bob")        // команда .ToLower() переводит все символы в нижний регистр
+{
+    Console.WriteLine("Oh my God, it's BOB!!!");
+}
+else
+{
+    Console.Write("Hello, "); Console.WriteLine(username);
+}
